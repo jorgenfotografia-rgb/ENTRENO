3878ce4c8e470bf17b0189857e52ea9e68a7358f
jorgenfotografia-rgb <jorgenfotografia@gmail.com>
2026-09-11 16:49:10 -0300

    chore: bump PRE-PILOT cache for V1.1.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pitbull-academy-core-v1-1-2-prepilot';
+const CACHE='pitbull-academy-core-v1-1-3-prepilot';
 const ASSETS=['./','./index.html','./styles.css','./core-v1.css','./app.js','./data/brands.js','./data/sources.js','./data/products.js','./data/modules.js','./data/clients.js','./data/scenarios/m01.js','./manifest.webmanifest',
 './assets/glutamina.webp','./assets/tiby-boss-check.webp','./assets/tiby-boss-review.webp','./assets/icon-192.png','./assets/icon-512.png','./assets/icon-512-maskable.png',
 './assets/tomas.svg','./assets/luciano.svg','./assets/marina.svg','./assets/matias.svg','./assets/carla.svg','./assets/federico.svg'];
