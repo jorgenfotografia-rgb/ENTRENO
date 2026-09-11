da58c3cf72b57e0d76c38ec490dc4b92f2bf4606
jorgenfotografia-rgb <jorgenfotografia@gmail.com>
2026-09-11 14:48:26 -0300

    core: add source registry

---
+++ data/sources.js
@@ -0,0 +1,11 @@
+window.SOURCES={
+  nutribull:{
+    id:'nutribull',
+    name:'Nutribull',
+    type:'retailer_catalog',
+    roleLabel:'fuente de catálogo',
+    url:'https://nutribull.com.ar/',
+    reviewedAt:'2026-09-11',
+    scope:'Inventario, presentación, sabores e imágenes de referencia. Las afirmaciones técnicas requieren auditoría independiente.'
+  }
+};
